e6f1551c8072132d601f0c7808a5a4b67f8a33ad
RabidOwlbear <owlbearaviary@gmail.com>
2023-08-09 17:42:25 -0700

    update 0.4.26

--- changelog.md
+++ changelog.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.4.26]
+### changes
+- Rations are now identified by a Ration tag. This allow any item with a "Ration" tag to be used by the eat ration feature. Ration items require a quantity of at least one "1/1".
+- Added a macro to migrate existing ration items in a game world to the new system. If an item on a character sheet is not appering in the use ration popup, make sure that the item has a "Ration" tag.
+
 ## [0.4.25]
 ### changes
 - Fixed issue preventing light item config menu from opening.
--- scripts/modules/rations.js
+++ scripts/modules/rations.js
@@ -1,38 +1,6 @@
 export const registerRations = () => {
   OSRH.ration = OSRH.ration || {};
 
-  // OSRH.ration.rationTrackingInit = async function () {
-  //   const foldernames = ['Active Characters', 'Active Retainers'];
-  //   const actorIds = [];
-  //   for (let name of foldernames) {
-  //     let folder = game.folders.getName(name);
-  //     if (!folder) {
-  //       folder = await Folder.create({
-  //         name: name,
-  //         type: 'Actor',
-  //         // parent: 'OMbaST8BhMtHx2zT',
-  //         color: '#006688'
-  //       });
-  //       console.log(`${OSRH.moduleName}: No ${name} Folder Found.
-  //   Folder Named ${name} Created.`);
-  //     }
-
-  //     //folder = game.folders.getName(folderName);
-  //     for (let actor of folder.content) {
-  //       actorIds.push(actor?.id);
-  //     }
-  //   }
-
-  //   return actorIds;
-  // };
-
-  // OSRH.ration.checkRations = async function (arr) {
-  //   for (let id of arr) {
-  //     const actor = game.actors.find((a) => a.id == id);
-  //   }
-  // };
-
-
   /* 
 data: {
   character: array containing player character names to be checked,
@@ -52,15 +20,22 @@ data: {
       }
     }
     const actor = await game.actors.find((a) => a.id == actorId);
+    // const rationItems = await actor.items.find()
     let rationOptions = '';
-    console.log("items", actor.items)
-    for (let type in OSRH.data.food) {
-      // console.log(type, OSRH.data.food[type])
-      const item = actor.items.getName(OSRH.data.food[type]);
-      if (item) {
-        rationOptions += `<option value="${item.name}">${item.name}: ${item.system.quantity.value}</option>`;
+    actor.items.map(i=>{
+      const isRat = i.system.tags.find(t=>t.title === 'Ration') ? true : false;
+      if(isRat){
+        rationOptions += `<option value="${i.name}">${i.name}: ${i.system.quantity.value}</option>`
       }
-    }
+    })
+    
+    // for (let type in OSRH.data.food) {
+    //   // console.log(type, OSRH.data.food[type])
+    //   const item = actor.items.getName(OSRH.data.food[type]);
+    //   if (item) {
+    //     rationOptions += `<option value="${item.name}">${item.name}: ${item.system.quantity.value}</option>`;
+    //   }
+    // }
 
     let dialogTemplate = `
   <h1> ${game.i18n.localize("OSRH.ration.pickType")}</h1>
@@ -76,7 +51,6 @@ data: {
           callback: async (html) => {
             let item = actor.items.getName(html.find('#ration')[0].value);
             let itemQty = item.system.quantity.value - 1;
-            console.log('item', itemQty)
             if (itemQty <= 0) {
               await item.delete();
             } else {
